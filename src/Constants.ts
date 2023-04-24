0cf90bc22c7e7b655a6db256731896cb66f5784a
Niihara Megumu <nhrmgm1223@gmail.com>
2023-04-24 12:02:56 +0900

    delete comment

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,6 +1,3 @@
-// NOTE: スクリプトプロパティの設定方法
-// [【GAS】コードにAPIトークンやIDのベタ書きを避ける（プロパティサービスの活用） - Qiita](https://qiita.com/massa-potato/items/2209ff367d65c5dd6181)
-
 export namespace Constants {
   /** Slackの In Coming Webhook URL */
   export const SLACK_URL = PropertiesService.getScriptProperties().getProperty('SLACK_URL')
